951b9b1cef8b7e46ff3624582e675788b3f875be
Tamirys <105079071+TamirysOliveira@users.noreply.github.com>
2024-05-23 01:15:19 -0300

    Update lendo_um_arq.py
    Inserção de alguns comentários

--- lendo_um_arq.py
+++ lendo_um_arq.py
@@ -1,6 +1,9 @@
-def ler_arquivo():
-    caminho_do_arquivo = r"C:\Users\Tamirys\Documents\INDICE_CORRESP.txt"  
+# -*- coding: utf-8 -*-
 
+def ler_arquivo():
+    caminho_do_arquivo = r"C:\Users\Tamirys\Documents\INDICE_CORRESP.txt"  #aqui estou buscando um arquivo de texto que fala sobre as funções INDICE e CORRESP do Excel
+    #Se for testar o código em sua máquina, substitua o caminho acima para o caminho para o seu arquivo
+    
     # Abrindo o arquivo
     arquivo = open(caminho_do_arquivo, 'r', encoding='utf-8')
     
